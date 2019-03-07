5880859975d122ed08bdfb095993ae393ddcecdd
Tarrai <sche0148@algonquinlive.com>
2019-03-07 18:15:51 -0500

    listview unfinished, but working

---
+++ REForm/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace REForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Property> properties = new List<Property>();
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Property
+    {
+        public string Name { get; set; }
+
+        public int PropertyID { get; set; }
+
+        public int PropertyTenantID { get; set; }
+
+        public int PropertyExpenseID { get; set; }
+
+
+
+
+
+        
+    }
+
+}
