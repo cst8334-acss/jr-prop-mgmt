81d2a129bec657358d2beda7111d02e1aad32441
Vijay <abhi0001@algonquinlive.com>
2019-03-12 02:49:36 -0400

    Completed the Create, Read and Delete from Grid.

--- REForm/REForm/MainWindow.xaml.cs
+++ REForm/REForm/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
 using REForm.Helpers;
+
 namespace REForm
 {
     /// <summary>
@@ -23,25 +24,36 @@ namespace REForm
     public partial class MainWindow : Window
     {
         public MySqlConnection connection;
+        
+        //Declaration for expense table
         public DataTable expensesDataTable;
         public String GetExpensesQuery;
 
+        //Declaration for tenant table
+        public DataTable tenantDataTable;
+        public String GetTenantQuery;
+        public String DeleteTenantQuery;
+
         public MainWindow()
         {
+            //Select queries to get data from database
             GetExpensesQuery = "select expense_name as Name, expense_cost as Cost from expenses";
-
+            GetTenantQuery = "select tenant.tenantId as 'Id', tenant.tenant_name as 'Name', tenant.unit_number as 'Unit#', tenant.square_feet as 'Square Feet', tenant.rent_per_sf as 'Rent per SF', tenant.monthly_rent as 'Monthly Rent', tenant.annual_rent as 'Annual Rent' from property.tenant;";
             InitializeComponent();
 
             //Database Connection
             var connectionString = "SERVER=localhost;PORT=3306;DATABASE=property;UID=root;PASSWORD=1234;";
-            this.connection = new MySqlConnection(connectionString);
+            connection = new MySqlConnection(connectionString);
 
             //Expenses Tab Logic
             expensesDataTable = new DataTable();
-
             CRUDHelper.LoadDataTable(expensesDataTable, GetExpensesQuery, connection);
-
             ExpenseGrid.DataContext = expensesDataTable;
+
+            //Tenant Tab Logic
+            tenantDataTable = new DataTable();
+            CRUDHelper.LoadDataTable(tenantDataTable, GetTenantQuery, connection);
+            TenantGrid.DataContext = tenantDataTable;
 
             Button addExpenseBtn = new Button
             {
@@ -49,24 +61,48 @@ namespace REForm
             };
             addExpenseBtn.Click += AddExpenseBtn_Click;
 
+            Button addTenantBtn = new Button
+            {
+                Name = "addTenantBtn"
+            };
+            addTenantBtn.Click += AddTenantBtn_Click; 
         }
 
+        //Adds new expense window on click event
         private void AddExpenseBtn_Click(object sender, RoutedEventArgs e)
         {
             AddExpenseWindow addWindow = new AddExpenseWindow(connection, this);
             addWindow.Show();
         }
-
+        //Performs real-time refresh here
         public void RefreshExpenseGrid()
         {
             CRUDHelper.LoadDataTable(expensesDataTable, GetExpensesQuery, connection);
             ExpenseGrid.DataContext = expensesDataTable;
         }
 
+        //Adds new tenant window on click event
         private void AddTenantBtn_Click(object sender, RoutedEventArgs e)
         {
-            addTenantWindow addWindow = new addTenantWindow(connection);
+            addTenantWindow addWindow = new addTenantWindow(connection, this);
             addWindow.Show();
+        }
+        //Performs real-time refresh here
+        public void RefreshTenantGrid()
+        {
+            CRUDHelper.LoadDataTable(tenantDataTable, GetTenantQuery, connection);
+            TenantGrid.DataContext = tenantDataTable;
+        }
+        //Delete the selected row from datagrid
+        private void DeleteTenantBtn_Click(object sender, RoutedEventArgs e)
+        {
+            tenantDataTable.Rows.RemoveAt(TenantGrid.SelectedIndex);
+            MessageBox.Show("Tenant Deleted");
+        }
+       
+        private void EditTenantBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- REForm/REForm/addTenantWindow.xaml.cs
+++ REForm/REForm/addTenantWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using REForm.Helpers;
 
 namespace REForm
 {
@@ -19,14 +21,78 @@ namespace REForm
     /// </summary>
     public partial class addTenantWindow : Window
     {
-        public addTenantWindow()
+        public MySqlConnection connection { get; set; }
+        public String TenantName { get; set; }
+        public Int16 UnitNumber { get; set; }
+        public Double SquareFeet  { get; set; }
+        public Double RentPerSF { get; set; }
+        public Double MonthlyRent { get; set; }
+        public Double AnnualRent { get; set; }
+        public Int16 TenantPropertyId { get; set; }
+        public DataGrid TenantGrid { get; set; }
+        public MainWindow mainWindow { get; set; }
+
+
+        public addTenantWindow(MySqlConnection connection, MainWindow mainWindow)
         {
             InitializeComponent();
+            this.connection = connection;
+            this.mainWindow = mainWindow;
+            DataContext = this;
+
+            Button saveBtn = new Button()
+            {
+                Name = "saveBtn"
+            };
+            saveBtn.Click += SaveBtn_Click;
+
+            Button cancelBtn = new Button
+            {
+                Name = "cancelBtn"
+            };
+            cancelBtn.Click += CancelBtn_Click;
         }
 
         private void CancelBtn_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TenantName = formTenantName.Text;
+            UnitNumber = Int16.Parse(formUnitForm.Text);
+            SquareFeet = Double.Parse(formSquareFeet.Text);
+            RentPerSF = Double.Parse(formRent.Text);
+            MonthlyRent = SquareFeet * RentPerSF;
+            AnnualRent = MonthlyRent * 12;
+            TenantPropertyId = 1;
+            var addTenantQuery = buildSaveQuery(TenantName,UnitNumber,SquareFeet,RentPerSF,MonthlyRent,AnnualRent,TenantPropertyId);
+
+            //Here we execute the sql statements
+            CRUDHelper.ExecuteQuery(addTenantQuery, connection);
+            MessageBox.Show("Tenant added");
+            //this will refresh the grid for real-time refresh
+            mainWindow.RefreshTenantGrid();
+            Close();
+
+        }
+        /// <summary>
+        /// This will build the query to insert the data in the database
+        /// </summary>
+        /// <param name="TenantName"></param>
+        /// <param name="UnitNumber"></param>
+        /// <param name="SquareFeet"></param>
+        /// <param name="RentPerSF"></param>
+        /// <param name="MonthlyRent"></param>
+        /// <param name="AnnualRent"></param>
+        /// <param name="TenantPropertyId"></param>
+        /// <returns></returns>
+        private String buildSaveQuery(String TenantName, Int16 UnitNumber, Double SquareFeet, Double RentPerSF, Double MonthlyRent, Double AnnualRent,Int16 TenantPropertyId)
+        {
+            var command = "INSERT INTO property.tenant (tenant_name,unit_number,square_feet,rent_per_sf,monthly_rent,annual_rent,tenant_propertyId)" +
+                         $"VALUES ('{TenantName}', {UnitNumber},{SquareFeet},{RentPerSF},{MonthlyRent},{AnnualRent},{TenantPropertyId});";
+            return command;
         }
     }
 }
