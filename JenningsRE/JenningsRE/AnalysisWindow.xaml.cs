c46c5826084e4164af74e3ee8d65eaa03d26fd7f
Tarrai <sche0148@algonquinlive.com>
2019-04-01 10:23:05 -0400

    Changed Up UI

--- JenningsRE/JenningsRE/AnalysisWindow.xaml.cs
+++ JenningsRE/JenningsRE/AnalysisWindow.xaml.cs
@@ -10,25 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace JenningsRE
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AnalysisWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AnalysisWindow : Window
     {
-        public MainWindow()
+        public AnalysisWindow()
         {
             InitializeComponent();
         }
 
-        private void switchScreen(object sender, RoutedEventArgs e)
+        private void SwitchScreen(object sender, RoutedEventArgs e)
         {
-            AnalysisWindow Analysis = new AnalysisWindow();
-            Analysis.Show();
+            MainWindow Main = new MainWindow();
+            Main.Show();
             Close();
         }
     }
