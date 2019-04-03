3c90d1c84966ccd9f3b9cbf5806eeefa705cfd9a
liam-england <engliam95@gmail.com>
2019-04-03 12:34:19 -0400

    Updated Add Expense Methods

--- JenningsRE/JenningsRE/MainWindow.xaml.cs
+++ JenningsRE/JenningsRE/MainWindow.xaml.cs
@@ -23,13 +23,41 @@ namespace JenningsRE
         public MainWindow()
         {
             InitializeComponent();
+
+
+            //Expense Tab Logic
+
+            //Add Expense Button
+            Button addExpenseBtn = new Button
+            {
+                Name = "addExpenseBtn"
+            };
+            addExpenseBtn.Click += AddExpenseBtn_Click;
         }
 
+        /// <summary>
+        /// Display Pop-up Add Expense Window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddExpenseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddExpense addWindow = new AddExpense();
+            addWindow.ShowDialog();
+        }
+
+        /// <summary>
+        /// Switch view to Analysis
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void switchScreen(object sender, RoutedEventArgs e)
         {
             AnalysisWindow Analysis = new AnalysisWindow();
             Analysis.Show();
             Close();
         }
+        
+
     }
 }
